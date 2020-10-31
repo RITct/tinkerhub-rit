8c18b8adf4700d0a1aee3c272cb27d24ec29b6e4
dravog7 <dravog77@gmail.com>
2020-10-31 19:32:37 +0530

    home base

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,17 @@
 <svelte:head>
-	<title>TinkerhubRIT</title>
+	<title>Tinkerhub-RIT</title>
 </svelte:head>
 
-<h1>Tinkerhub RIT</h1>
+<style>
+.full{
+	width: 100%;
+	height: 100%;
+	display: flex;
+	justify-content: center;
+}
+</style>
+
+<div class="full">
+	<h1>Coming soon!</h1>
+</div>
+
